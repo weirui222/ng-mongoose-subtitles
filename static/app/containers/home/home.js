e019ad832ac4287cb29902c0564326ea7b3f673c
Travis Walentin <travis.walentin@gmail.com>
2017-01-23 11:38:00 -0800

    added home page list to render and have links.

--- static/app/containers/home/home.js
+++ static/app/containers/home/home.js
@@ -6,6 +6,38 @@ angular.module('App')
 });
 
 function HomeCompCtrl() {
+    //static test data
+
+
+    var homeComp = this;
+    homeComp.titles = [];
+    //this is where you would get the things from the db
+    //TODO: require the services in the function and below
+    //Service.getSubtitles().then();
+
+    homeComp.titles = [
+      {
+        "_id":'1',
+        "title": "Original"
+      },
+      {
+        "_id":'2',
+        "title": "WDI 11"
+      },
+      {
+        "_id":'3',
+        "title": "Blue"
+      },
+      {
+        "_id":'4',
+        "title": "Red"
+      },
+      {
+        "_id":'5',
+        "title": "Grey"
+      },
+
+    ];
 
 }
 
